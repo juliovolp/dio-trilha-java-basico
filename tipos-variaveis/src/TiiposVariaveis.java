194e0e806fff40e691e4bb49ac2ae4e5ec68ac95
juliovolp <julio.volpatto@gmail.com>
2023-09-04 16:59:42 -0300

    Final tipo de variaveis

---
+++ tipos-variaveis/src/TiiposVariaveis.java
@@ -0,0 +1,19 @@
+public class TiiposVariaveis {
+    public static void main(String[] args) throws Exception {
+      //tipos primitivos
+      //Classe string
+
+      String meuNome = "Julio Volpatto";
+        double salarioMinimo = 2500.33;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero =5;
+        numero =10;
+        System.out.println(numero);
+        
+        final double VALOR_DE_PI = 3.14;
+    }
+}
